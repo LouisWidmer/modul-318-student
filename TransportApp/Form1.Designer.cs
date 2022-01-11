52d8912c4a09255f49300bf2485ba86123a1601a
Louis Widmer <Louis_Widmer@sluz.ch>
2022-01-11 15:57:20 +0100

    verbindungssuche implementiert

--- TransportApp/Form1.Designer.cs
+++ TransportApp/Form1.Designer.cs
@@ -36,15 +36,21 @@
             this.label3 = new System.Windows.Forms.Label();
             this.btnSuchen = new System.Windows.Forms.Button();
             this.btnIdSuchen = new System.Windows.Forms.Button();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
             this.gridAbfahrten = new System.Windows.Forms.DataGridView();
-            this.btnAufKarteAnzeigen = new System.Windows.Forms.Button();
-            this.btnMailSenden = new System.Windows.Forms.Button();
             this.Gleis = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Abfahrt = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Ankunft = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Dauer = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Verzogerung = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAufKarteAnzeigen = new System.Windows.Forms.Button();
+            this.btnMailSenden = new System.Windows.Forms.Button();
+            this.gridStationBoard = new System.Windows.Forms.DataGridView();
+            this.Von = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nach = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.gridAbfahrten)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridStationBoard)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -72,6 +78,7 @@
             this.combVon.Name = "combVon";
             this.combVon.Size = new System.Drawing.Size(224, 28);
             this.combVon.TabIndex = 2;
+            this.combVon.DropDown += new System.EventHandler(this.combVon_DropDown);
             // 
             // combNach
             // 
@@ -80,6 +87,7 @@
             this.combNach.Name = "combNach";
             this.combNach.Size = new System.Drawing.Size(224, 28);
             this.combNach.TabIndex = 3;
+            this.combNach.DropDown += new System.EventHandler(this.combNach_DropDown);
             // 
             // btnNachsteStation
             // 
@@ -89,6 +97,7 @@
             this.btnNachsteStation.TabIndex = 4;
             this.btnNachsteStation.Text = "Suche nächste Station";
             this.btnNachsteStation.UseVisualStyleBackColor = true;
+            this.btnNachsteStation.Click += new System.EventHandler(this.btnNachsteStation_Click);
             // 
             // label3
             // 
@@ -118,12 +127,13 @@
             this.btnIdSuchen.Text = "Per ID suchen";
             this.btnIdSuchen.UseVisualStyleBackColor = true;
             // 
-            // dateTimePicker1
+            // dateTimePicker
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(337, 40);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(224, 27);
-            this.dateTimePicker1.TabIndex = 9;
+            this.dateTimePicker.Location = new System.Drawing.Point(337, 40);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(224, 27);
+            this.dateTimePicker.TabIndex = 9;
+            this.dateTimePicker.Value = new System.DateTime(2022, 1, 1, 0, 0, 0, 0);
             // 
             // gridAbfahrten
             // 
@@ -134,31 +144,14 @@
             this.Gleis,
             this.Abfahrt,
             this.Ankunft,
-            this.Dauer});
+            this.Dauer,
+            this.Verzogerung});
             this.gridAbfahrten.Location = new System.Drawing.Point(37, 121);
             this.gridAbfahrten.Name = "gridAbfahrten";
             this.gridAbfahrten.RowHeadersWidth = 51;
             this.gridAbfahrten.RowTemplate.Height = 29;
             this.gridAbfahrten.Size = new System.Drawing.Size(615, 462);
             this.gridAbfahrten.TabIndex = 10;
-            // 
-            // btnAufKarteAnzeigen
-            // 
-            this.btnAufKarteAnzeigen.Location = new System.Drawing.Point(37, 589);
-            this.btnAufKarteAnzeigen.Name = "btnAufKarteAnzeigen";
-            this.btnAufKarteAnzeigen.Size = new System.Drawing.Size(242, 29);
-            this.btnAufKarteAnzeigen.TabIndex = 11;
-            this.btnAufKarteAnzeigen.Text = "Auf der Karte anzeigen";
-            this.btnAufKarteAnzeigen.UseVisualStyleBackColor = true;
-            // 
-            // btnMailSenden
-            // 
-            this.btnMailSenden.Location = new System.Drawing.Point(410, 589);
-            this.btnMailSenden.Name = "btnMailSenden";
-            this.btnMailSenden.Size = new System.Drawing.Size(242, 29);
-            this.btnMailSenden.TabIndex = 12;
-            this.btnMailSenden.Text = "Per Mail versenden";
-            this.btnMailSenden.UseVisualStyleBackColor = true;
             // 
             // Gleis
             // 
@@ -192,15 +185,81 @@
             this.Dauer.Name = "Dauer";
             this.Dauer.ReadOnly = true;
             // 
+            // Verzogerung
+            // 
+            this.Verzogerung.HeaderText = "Verzögerung";
+            this.Verzogerung.MinimumWidth = 6;
+            this.Verzogerung.Name = "Verzogerung";
+            this.Verzogerung.ReadOnly = true;
+            this.Verzogerung.Width = 125;
+            // 
+            // btnAufKarteAnzeigen
+            // 
+            this.btnAufKarteAnzeigen.Location = new System.Drawing.Point(37, 589);
+            this.btnAufKarteAnzeigen.Name = "btnAufKarteAnzeigen";
+            this.btnAufKarteAnzeigen.Size = new System.Drawing.Size(242, 29);
+            this.btnAufKarteAnzeigen.TabIndex = 11;
+            this.btnAufKarteAnzeigen.Text = "Auf der Karte anzeigen";
+            this.btnAufKarteAnzeigen.UseVisualStyleBackColor = true;
+            // 
+            // btnMailSenden
+            // 
+            this.btnMailSenden.Location = new System.Drawing.Point(410, 589);
+            this.btnMailSenden.Name = "btnMailSenden";
+            this.btnMailSenden.Size = new System.Drawing.Size(242, 29);
+            this.btnMailSenden.TabIndex = 12;
+            this.btnMailSenden.Text = "Per Mail versenden";
+            this.btnMailSenden.UseVisualStyleBackColor = true;
+            // 
+            // gridStationBoard
+            // 
+            this.gridStationBoard.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gridStationBoard.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridStationBoard.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Von,
+            this.Nach,
+            this.dataGridViewTextBoxColumn1});
+            this.gridStationBoard.Location = new System.Drawing.Point(37, 121);
+            this.gridStationBoard.Name = "gridStationBoard";
+            this.gridStationBoard.RowHeadersWidth = 51;
+            this.gridStationBoard.RowTemplate.Height = 29;
+            this.gridStationBoard.Size = new System.Drawing.Size(615, 462);
+            this.gridStationBoard.TabIndex = 13;
+            // 
+            // Von
+            // 
+            this.Von.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Von.HeaderText = "Von";
+            this.Von.MinimumWidth = 6;
+            this.Von.Name = "Von";
+            this.Von.ReadOnly = true;
+            // 
+            // Nach
+            // 
+            this.Nach.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Nach.HeaderText = "Nach";
+            this.Nach.MinimumWidth = 6;
+            this.Nach.Name = "Nach";
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn1.HeaderText = "Abfahrt";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(694, 630);
+            this.Controls.Add(this.gridStationBoard);
             this.Controls.Add(this.btnMailSenden);
             this.Controls.Add(this.btnAufKarteAnzeigen);
             this.Controls.Add(this.gridAbfahrten);
-            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dateTimePicker);
             this.Controls.Add(this.btnIdSuchen);
             this.Controls.Add(this.btnSuchen);
             this.Controls.Add(this.label3);
@@ -211,7 +270,9 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.gridAbfahrten)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridStationBoard)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -227,7 +288,7 @@
         private Label label3;
         private Button btnSuchen;
         private Button btnIdSuchen;
-        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePicker;
         private DataGridView gridAbfahrten;
         private Button btnAufKarteAnzeigen;
         private Button btnMailSenden;
@@ -235,5 +296,10 @@
         private DataGridViewTextBoxColumn Abfahrt;
         private DataGridViewTextBoxColumn Ankunft;
         private DataGridViewTextBoxColumn Dauer;
+        private DataGridViewTextBoxColumn Verzogerung;
+        private DataGridView gridStationBoard;
+        private DataGridViewTextBoxColumn Von;
+        private DataGridViewTextBoxColumn Nach;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
     }
 }
--- TransportApp/Form1.cs
+++ TransportApp/Form1.cs
@@ -9,8 +9,87 @@ namespace TransportApp
 
         private void btnSuchen_Click(object sender, EventArgs e)
         {
+            gridAbfahrten.Rows.Clear();
             SwissTransport.Core.ITransport transport = new SwissTransport.Core.Transport();
-            transport.GetStations("Luzern");
+            if (combVon.Text.Length >= 3)
+            {
+                if (combNach.Text == "")
+                {
+                    SwissTransport.Models.StationBoardRoot stationBoardRoot;
+                    stationBoardRoot = transport.GetStationBoard(combVon.Text, "0");
+                    gridAbfahrten.Hide();
+                    gridStationBoard.Show();
+                    gridStationBoard.Rows.Clear();
+                    for (int i = 0; i < stationBoardRoot.Entries.Count; i++)
+                    {
+                        string[] text = new string[3];
+                        text[0] = stationBoardRoot.Station.Name;
+                        text[1] = stationBoardRoot.Entries[i].To;
+                        text[2] = stationBoardRoot.Entries[i].Stop.Departure.ToString();
+                        gridStationBoard.Rows.Add(text);
+                    }
+                }
+                else
+                {
+                    SwissTransport.Models.Connections connections = new SwissTransport.Models.Connections();
+                    connections = transport.GetConnections(combVon.Text, combNach.Text);
+                    gridStationBoard.Hide();
+                    gridAbfahrten.Show();
+                    for (int i = 0; i < connections.ConnectionList.Count; i++)
+                    {
+                        string[] text = new string[5];
+                        text[0] = connections.ConnectionList[i].From.Platform;
+                        text[1] = connections.ConnectionList[i].From.Departure.ToString();
+                        text[2] = connections.ConnectionList[i].To.Arrival.ToString();
+                        text[3] = Convert.ToString(connections.ConnectionList[i].To.Arrival - connections.ConnectionList[i].From.Departure);
+                        text[4] = Convert.ToString(connections.ConnectionList[i].From.Delay);
+                        gridAbfahrten.Rows.Add(text);
+                    }
+                }
+            }
+        }
+
+        private void combVon_DropDown(object sender, EventArgs e)
+        {
+            if (combVon.Text.Length >= 3)
+            {
+                combVon.Items.Clear();
+                combVon.Items.Add(combVon.Text);
+                SwissTransport.Core.ITransport transport = new SwissTransport.Core.Transport();
+                SwissTransport.Models.Stations stations = new SwissTransport.Models.Stations();
+                stations = transport.GetStations(combVon.Text);
+                for (int i = 0; i < stations.StationList.Count; i++)
+                {
+                    combVon.Items.Add(stations.StationList[i].Name);
+                }
+            }
+        }
+
+        private void combNach_DropDown(object sender, EventArgs e)
+        {
+            if (combNach.Text.Length >= 3)
+            {
+                combNach.Items.Clear();
+                combNach.Items.Add(combNach.Text);
+                SwissTransport.Core.ITransport transport = new SwissTransport.Core.Transport();
+                SwissTransport.Models.Stations stations = new SwissTransport.Models.Stations();
+                stations = transport.GetStations(combNach.Text);
+                for (int i = 0; i < stations.StationList.Count; i++)
+                {
+                    combNach.Items.Add(stations.StationList[i].Name);
+                }
+            }
+        }
+
+        private void btnNachsteStation_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gridAbfahrten.Hide();
+            gridStationBoard.Hide();
         }
     }
 }
